562b6fc54e68f180045d1c907a5b424a079d7dcd
reopesyh <hacktest9604@gmail.com>
2024-09-06 07:26:03 +0530

    Update hr.py

--- hr.py
+++ hr.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '6402878130:AAEFMSXSH8Gs5efzuPmsl2M788T-JQOQV5E'
+ADMIN_ID = '1381828828'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
